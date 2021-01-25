887c77bb9c879829ef2329f3cc817045cd50a0d4
JWBstuhr <77174240+JWBstuhr@users.noreply.github.com>
2021-01-25 12:22:01 -0600

    Update JWB.OS-WGet-Installer.lua

--- JWB.OS-WGet-Installer.lua
+++ JWB.OS-WGet-Installer.lua
@@ -12,5 +12,7 @@ shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.chath
 shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.chatj.lua os/.chatj")
 shell.run("wget https://raw.githubusercontent.com/JWBstuhr/JWB.OS/main/os/.uninstall.lua os/.uninstall")
 print("Done. Rebooting...")
+local installname = shell.getRunningProgram()
+shell.run("rm "..installname)
 sleep(2)
 os.reboot()
